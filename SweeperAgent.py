6ce05d0fa15d70d97190a984cc305d557940e0e2
Birkir K <birkir17@ru.is>
2019-03-29 17:51:22 +0000

    generate random first move

--- SweeperAgent.py
+++ SweeperAgent.py
@@ -1,4 +1,16 @@
+import itertools
+import random
+import string
+
 def SweeperAgent(grid):
-    print('From agent')
-    print(grid)
+    firstMove = True
+    letters = string.ascii_lowercase
+    for item in itertools.chain.from_iterable(grid):
+        if item is not ' ': firstMove = False
+    if(firstMove):
+        a = random.randint(0, len(grid) - 1)
+        b = random.randint(0, len(grid) - 1)
+        print(a, b)
+        print(letters[a] + str(b))
+    #print('First move:', firstMove)
 
